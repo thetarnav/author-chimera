72ee77cfc5dd13af13a6e309691af64bf07f8f52
Damian Tarnawski <gthetarnav@gmail.com>
2019-08-13 16:44:02 +0200

    Endless loop is now working on mobile.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,63 @@
-// const bodyScrollLock = require('body-scroll-lock'),
-//    disableBodyScroll = bodyScrollLock.disableBodyScroll;
+const bodyScrollLock = require('body-scroll-lock');
+bodyScrollLock.disableBodyScroll(window);
 
-// disableBodyScroll(window);
+const SVGInject = require('@iconfu/svg-inject');
+const debounce = require('lodash.debounce');
+
+
+
+
+const main = document.querySelector('#main'),
+   mainCopy = document.querySelector('#main-copy'); 
+   
+let tl,
+   lastTouchY;
+
+window.onload = () => {
+   TweenLite.set(document.body, {scrollTo: 0});
+};
+
+window.addEventListener('touchstart', e => lastTouchY = e.touches[0].clientY);
+
+window.addEventListener('touchmove', debounce(e => {
+   let distToMove = 60;
+
+   e.touches[0].clientY - distToMove > lastTouchY && scrollUp();
+   e.touches[0].clientY + distToMove < lastTouchY && scrollDown();
+   lastTouchY = e.touches[0].clientY;
+},
+   30, {
+      leading: true,
+      maxWait: 30
+   }
+));
+
+function scrollDown() {
+   if (typeof tl != 'undefined' && tl.isActive()) return;
+
+   tl = new TimelineLite();
+   tl.set(document.body, {
+         scrollTo: 0
+      })
+      .to(document.body, .5, {
+         ease: Power2.easeInOut,
+         scrollTo: mainCopy.offsetTop
+      })
+      .set(document.body, {
+         scrollTo: 0
+      });
+}
+
+function scrollUp() {
+   if (typeof tl != 'undefined' && tl.isActive()) return;
+
+   tl = new TimelineLite();
+   tl.set(document.body, {
+         scrollTo: mainCopy.offsetTop
+      })
+      .to(document.body, .5, {
+         ease: Power2.easeInOut,
+         scrollTo: 0
+      });
+}
 
